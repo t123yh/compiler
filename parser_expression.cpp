759ea8e72cc3b0c87b5534f50d5202d341b4a45c
Yunhao Tian <t123yh@outlook.com>
2020-10-29 19:44:19 +0800

    make_unique -> utils

--- parser_expression.cpp
+++ parser_expression.cpp
@@ -7,11 +7,6 @@
 #include "parser_const.h"
 #include "parser_function.h"
 
-template<typename T, typename... Args>
-std::unique_ptr<T> make_unique(Args &&... args) {
-    return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
-}
-
 expression_parser::return_type expression_parser::parse(parsing_context &context) const {
     std::unique_ptr<expression> current;
     if (context.parse_if_match(token_parser<MINU>())) {
--- utils.h
+++ utils.h
@@ -7,4 +7,9 @@
 
 #define COUNT_OF(x) ((sizeof(x)/sizeof(0[x])) / ((size_t)(!(sizeof(x) % sizeof(0[x])))))
 
+template<typename T, typename... Args>
+std::unique_ptr<T> make_unique(Args &&... args) {
+    return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+}
+
 #endif //COMPILER_UTILS_H
